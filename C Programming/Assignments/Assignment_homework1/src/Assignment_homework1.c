54a07fefe850325af6385b62366f72c3f22aa90d
ashraf mohamed <amwushu99@gmail.com>
2023-07-15 11:39:23 +0200

    adding the Assignments

---
+++ C Programming/Assignments/Assignment_homework1/src/Assignment_homework1.c
@@ -0,0 +1,110 @@
+/*
+ ============================================================================
+ Name        : Assignment_homework1.c
+ Author      : ashraf mohamed 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+//	EX1:
+   /*
+ 	printf("C Programming");
+ 	*/
+
+
+//  EX2:
+   /*
+	int number;
+	printf("Enter a integer: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&number);
+	printf("You entered: %d",number);
+   */
+
+
+
+//  EX3:
+  /*
+	int num1,num2;
+	printf("Enter two integers: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d\n%d",&num1,&num2);
+	printf("Sum: %d",num1+num2);
+  */
+
+//  EX4:
+   /*
+	float num1,num2;
+	printf("Enter two numbers: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f\n%f",&num1,&num2);
+	printf("Product: %f",num1*num2);
+   */
+
+
+//  EX5:
+   /*
+	char num1;
+	printf("Enter a character: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%c",&num1);
+	printf("ASCII value of %c = %d",num1,num1);
+   */
+
+//  EX6:
+   /*
+	float a,b,temp;
+	printf("Enter value of a: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f",&a);
+
+	printf("Enter value of b: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f",&b);
+
+	temp=a;
+	a=b;
+	b=temp;
+
+	printf("After swapping, value of a = %f\n",a);
+	printf("After swapping, value of b = %f",b);
+   */
+
+
+//  EX7:
+   /*
+	float a,b;
+	printf("Enter value of a: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f",&a);
+
+	printf("Enter value of b: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%f",&b);
+
+	a=a+b;
+	b=a-b;
+	a=a-b;
+
+	printf("After swapping, value of a = %f\n",a);
+	printf("After swapping, value of b = %f",b);
+   */
+
+	return EXIT_SUCCESS;
+}
