27ab46d659c27028d42874841ae28bbf07ac1da4
ashraf mohamed <amwushu99@gmail.com>
2023-11-19 09:19:13 +0200

    assginment 1

---
+++ Embedded C/Lesson 1/embedded_c1/Inc/Platforms_Types.h
@@ -0,0 +1,43 @@
+/*
+ * Platforms_Types.h
+ *
+ *  Created on: ١٧‏/١١‏/٢٠٢٣
+ *      Author: ashraf
+ */
+
+
+#ifndef PLATFORMS_TYPES_H_
+#define PLATFORMS_TYPES_H_
+
+
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
+
+
+
+
+typedef volatile int8_t vint8_t;
+typedef volatile uint8_t vuint8_t;
+
+typedef volatile int16_t vint16_t;
+typedef volatile uint16_t vuint16_t;
+
+typedef volatile int32_t vint32_t;
+typedef volatile uint32_t vuint32_t;
+
+typedef volatile int64_t vint64_t;
+typedef volatile uint64_t vuint64_t;
+
+
+#endif /* PLATFORMS_TYPES_H_ */
+
